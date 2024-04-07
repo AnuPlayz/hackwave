bb655fd9c947bfe24476034c9d9af141d148dadd
Aniruddh <adubge@gmail.com>
2024-04-07 22:13:45 +0530

    Update team members in data.js

--- src/Pages/Team/data.js
+++ src/Pages/Team/data.js
@@ -3,13 +3,31 @@ const teamsData = [
     name: "ORGANIZER",
     members: [
       {
+        name: "Anam Mansoori",
+        image:
+          "./Team/anam.jpeg",
+        email: "anammansoori@gmail.com",
+        linkedin: "https://www.linkedin.com/in/anam-mansoori-9a46482b3/",
+        twitter: "",
+        instagram: "https://www.instagram.com/anam.03jv/",
+      },
+      {
         name: "Aniruddh Dubge",
         image:
-          "./Team/picofme.png",
+          "./Team/ani.jpeg",
         email: "adubge@gmail.com",
         linkedin: "https://www.linkedin.com/in/ani__x_/",
-        twitter: "",
+        twitter: "https://twitter.com/ani__x_",
         instagram: "https://www.instagram.com/ani__x_/",
+      },
+      {
+        name: "Atharvaraj Thakur",
+        image:
+          "./Team/atharva.jpeg",
+        email: "atharvaraj@gmail.com",
+        linkedin: "https://www.linkedin.com/in/atharvarajthakur/",
+        twitter: "",
+        instagram: "https://www.instagram.com/atharvarajthakur",
       },
     ],
   },
